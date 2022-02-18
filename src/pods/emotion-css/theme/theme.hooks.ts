a6310e95c63625f672b9c974e457b1a22d98484b
nasdan <daniel.sanchez@lemoncode.net>
2022-02-18 15:46:21 +0100

    fix required prop when needed

--- src/pods/emotion-css/theme/theme.hooks.ts
+++ src/pods/emotion-css/theme/theme.hooks.ts
@@ -8,22 +8,30 @@ export type ResultType<Classes> = {
 
 export type PropsType<Classes, K extends keyof Classes = keyof Classes> = {
   [Key in K]: Classes[Key] extends (arg1: any, arg2: any) => string
-    ? Parameters<Classes[Key]>[1]
+    ? Parameters<Classes[Key]>[1] extends undefined
+      ? never
+      : Parameters<Classes[Key]>[1]
     : Classes[Key] extends string
-    ? undefined
+    ? never
     : PropsType<Classes[Key]>;
 }[K];
 
+export type ClassesHasProps<Classes> = PropsType<Classes> extends never
+  ? false
+  : true;
+
 export const useWithTheme = <Classes>(
   classes: Classes,
-  props?: PropsType<Classes>
+  ...rest: ClassesHasProps<Classes> extends true
+    ? [props: PropsType<Classes>]
+    : []
 ): ResultType<Classes> => {
   const theme = useTheme();
 
   return Object.entries(classes).reduce(
     (result, [key, value]) => ({
       ...result,
-      [key]: typeof value === 'function' ? value(theme, props) : value,
+      [key]: typeof value === 'function' ? value(theme, rest[0]) : value,
     }),
     {} as ResultType<Classes>
   );
